9167499dc010e54df204766d74c99412241862ca
tanveer <mdtanviralamht731@gmail.com>
2022-04-22 08:42:27 +0600

    user deleted and and UI updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+  const handleUserDelete = (id) => {
+    const proceed = window.confirm("Are You sure?");
+    if (proceed) {
+      console.log("user id", id);
+      const url = `http://localhost:5000/user/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaining = users.filter((user) => user._id !== id);
+          setUsers(remaining);
+        });
+    }
+  };
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name}== {user.email}
+            <button onClick={() => handleUserDelete(user._id)}>-</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
